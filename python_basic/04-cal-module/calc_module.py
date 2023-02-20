7ddc3835afc77394996ff934621ab3ff825ea367
arastogi <arastogi@clearwateranalytics.com>
2023-02-20 20:39:58 +0530

    decorator added

---
+++ python_basic/04-cal-module/calc_module.py
@@ -0,0 +1,24 @@
+#this is calc module as written by ayush
+#and using by demo.py
+def add(a,b):
+    return a+b
+
+def sub(a,b):
+    return a-b
+
+def multi(a,b):
+    return a*b
+
+def div(a,b):
+    return a/b
+
+def main():
+    print("hello from calc")
+
+'''
+It Allows You to Execute Code When the File Runs as a Script, 
+but Not When It's Imported as a Module.
+'''
+
+if __name__ == "__main__" :
+    main()
---
+++ python_basic/04-cal-module/demo.py
@@ -0,0 +1,14 @@
+#using calc_module.py module 
+
+from calc_module import *
+
+a=9
+b=7
+
+c=add(a,b)
+print(c)
+
+#d=multi(a,b)
+#print(d)
+#get my module name which is main always, point of executions
+
